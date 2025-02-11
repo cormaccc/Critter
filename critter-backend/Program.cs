b48b47ffc77137115bf023b9e768e99db6611dc0
Cormac Dunne <dunne.cormac@hotmail.co.uk>
2025-02-11 20:44:28 +0000

    Added NSwag to create front-end api service

--- critter-backend/Program.cs
+++ critter-backend/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
